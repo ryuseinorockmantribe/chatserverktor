8cbf09159fdb474621943f47a6c8a442eaf6b2bc
Jose Alberto Aramburo Lucero <josealbertoaramburolucero@Joses-MacBook-Air.local>
2023-07-13 23:08:35 -0600

    Mensaje del commit

--- src/main/kotlin/com/plcoding/di/MainModule.kt
+++ src/main/kotlin/com/plcoding/di/MainModule.kt
@@ -14,7 +14,7 @@ val connectionString = "mongodb://naisho:naisho2011@lan-2870115-lan:27017"
 
 val mainModule = module {
     single {
-        KMongo.createClient()
+        KMongo.createClient(connectionString)
             .coroutine
             .getDatabase("message_db_yt")
     }
