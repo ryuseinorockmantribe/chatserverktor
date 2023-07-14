4fd15d12ae7fcd8c1388d16c3ea491bf4726edc2
Jose Alberto Aramburo Lucero <josealbertoaramburolucero@Joses-MacBook-Air.local>
2023-07-13 22:54:25 -0600

    Mensaje del commit

--- src/main/kotlin/com/plcoding/di/MainModule.kt
+++ src/main/kotlin/com/plcoding/di/MainModule.kt
@@ -7,6 +7,7 @@ import org.koin.dsl.module
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
+
 val mainModule = module {
     single {
         KMongo.createClient()
